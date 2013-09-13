32715a85975d5d22072a19bcf05194370c518610
Stephen Suen <ssuen@mit.edu>
2013-09-13 15:34:34 -0400

    Reformat graphics.js for stylistic consistency.

--- graphics.js
+++ graphics.js
@@ -1,17 +1,17 @@
 // a very simple graphics library using HTML5 canvas features
 
 // constructor for 2D coordinate
-var Coord = function (x, y) {
+var Coord = function(x, y) {
 	return {x:x, y:y};
-	};
+};
 
 // constructor for color
-var Color = function (red, green, blue) {
+var Color = function(red, green, blue) {
 	return {red: red, green: green, blue: blue};
-	};
+};
 
 // an abstraction for drawing in a canvas
-var Pad = function (canvas) {
+var Pad = function(canvas) {
 	var DEFAULT_CIRCLE_RADIUS = 5;
 	var DEFAULT_LINE_WIDTH = 2;
 
@@ -20,24 +20,24 @@ var Pad = function (canvas) {
 	var height = canvas.height;
 
 	// sets the line width for subsequent drawing
-	var apply_line_width = function (ctx, line_width) {
+	var apply_line_width = function(ctx, line_width) {
 		ctx.lineWidth = (line_width) ? line_width : DEFAULT_LINE_WIDTH;
-		}
+	};
 
 	// sets the color for subsequent drawing and a default stroke width
-	var apply_color = function (ctx, color) {
+	var apply_color = function(ctx, color) {
 		if (color) {
 			ctx.strokeStyle = 'rgba(' + color.red + ',' + color.green + ',' + color.blue + ', 1)';
-			}
 		}
+	};
 
 	// sets the fill color for subsequent drawing
-	var apply_fill_color = function (ctx, color) {
+	var apply_fill_color = function(ctx, color) {
 		if (color) {
 			ctx.fillStyle = 'rgba(' + color.red + ',' + color.green + ',' + color.blue + ', 1)';
 			ctx.fill();
-			}
 		}
+	};
 
 	// return the abstract object from the constructor
 
@@ -57,7 +57,7 @@ var Pad = function (canvas) {
 			apply_color(context, color);
 			apply_fill_color(context, fill_color);
 			context.stroke();
-			},
+		},
 
 		// Draws a line between the given coordinates (as returned by
 		// the Coord function). An optional line width can be supplied
@@ -72,7 +72,7 @@ var Pad = function (canvas) {
 			context.lineWidth = (line_width) ? line_width : DEFAULT_LINE_WIDTH;
 			context.closePath();
 			context.stroke();
-			},
+		},
 
 		// Draws a rectangle starting at the top left corner (as
 		// returned by the Coord function) of the given width and
@@ -87,19 +87,19 @@ var Pad = function (canvas) {
 			apply_fill_color(context, fill_color);
 			context.closePath();
 			context.stroke();
-			},
+		},
 
 		// Clears the entire board
 		clear: function() {
 			context.clearRect(0, 0, width, height);
-			},
+		},
 
 		// return width and height of the drawing area
 		get_width: function() {
 			return width;
-			},
+		},
 		get_height: function() {
 			return height;
-			}
-	}
-}
+		}
+	};
+};
