8c4ba44441db83512bec339f4fce5a96121da2e8
Stephen Suen <ssuen@mit.edu>
2013-09-14 17:45:48 -0400

    Implement loop abstractions.

--- game.js
+++ game.js
@@ -8,11 +8,15 @@ var Game = function(r, c, s) {
 	var DEFAULT_ROWS = 10;
 	var DEFAULT_COLS = 10;
 
+	// import utility functions
+	var utils = Util();
+
 	// validate and set start conditions
 	var rows = (r && typeof r === 'number') ? r : DEFAULT_ROWS;
 	var cols = (c && typeof c === 'number') ? c : DEFAULT_COLS;
 	var live_cells = [];
 
+	//TODO: rewrite this whole thing
 	if (s && s instanceof Array) {
 		for (var i = 0; i < s.length; i++) {
 			if (s[i] instanceof Array && s[i].length === 2 && typeof s[0] === 'number' && typeof s[1] === 'number') {
@@ -22,41 +26,47 @@ var Game = function(r, c, s) {
 		}
 	}
 
-	if (live_cells.length === 0 ) {
-		live_cells = [[Math.floor(cols/2), Math.floor(rows/2)]]; // defaults to most central cell
+	if (live_cells.length === 0) {
+		live_cells = [[0,0], [Math.floor(cols/2), Math.floor(rows/2)]]; // defaults to most central cell
 	}
 
 	return {
-		// returns current grid
-		grid: function() {
-			var arr = [];
-			var std_col = [];
-			var i = cols;
-			var j = rows;
+		// returns current board
+		board: function() {
+			var grid = [];
+			var zero_col = [];
 
-			// populate grid with zeros
-			while (j--) {
-				std_col.push(0);
-			}
-			while (i--) {
-				arr.push(std_col.slice());
-			}
+			// create grid of dead cells
+			utils.from_to(0, rows - 1, function() {
+				zero_col.push(0);
+			});
+			utils.from_to(0, cols - 1, function() {
+				grid.push(zero_col.slice());
+			});
 
 			// populate live cells
-			for (var n = 0; n < live_cells.length; n++) {
-				var x = live_cells[n][0];
-				var y = live_cells[n][1];
-				arr[x][y] = 1;
-			}
+			utils.each(live_cells, function(cell) {
+				grid[cell[0]][cell[1]] = 1;
+			});
 
-			return arr;
+			return grid;
 		},
 
-		// returns state of a particular cell
+		// return cell state at (x, y)
 		cell: function(x, y) {
-			return this.grid()[x][y];
+			return this.board()[x][y];
 		},
 
+		// return coords of (x, y)'s neighbors
+		neighbors: function(x, y) {
+			var neighborhood = [];
+			utils.from_to_2d(Math.max(0, x - 1), Math.min(x + 1, cols), Math.max(0, y - 1), Math.min(y + 1, rows), function(cell) {
+				if (cell[0] !== x || cell[1] !== y) neighborhood.push(cell); // exclude self
+			});
+			return neighborhood;
+		},
+
+		// state transition
 		step: function() {
 			//TODO: iterate!
 		}
---
+++ util.js
@@ -0,0 +1,41 @@
+// utility functions for general use
+var Util = function() {
+	return {
+		// iteration abstraction (by Daniel Jackson)
+		from_to: function(from, to, f) {
+			if (from > to) return;
+			f(from);
+			this.from_to(from + 1, to, f);
+		},
+
+		// two-dimensional iteration abstraction
+		from_to_2d: function(from1, to1, from2, to2, f) {
+			var from2_init = from2;
+			var subprocess = function(from1, to1, from2, to2, f) {
+				if (from1 > to1) return;
+				f([from1, from2]);
+				if (from2 === to2) {
+					subprocess(from1 + 1, to1, from2_init, to2, f);
+				}
+				else {
+					subprocess(from1, to1, from2 + 1, to2, f);
+				}
+			};
+			if (from1 > to1) return;
+			f([from1, from2]);
+			if (from2 === to2) {
+				subprocess(from1 + 1, to1, from2, to2, f);
+			}
+			else {
+				subprocess(from1, to1, from2 + 1, to2, f);
+			}
+		},
+
+		// element iterator (by Daniel Jackson)
+		each: function(a, f) {
+			this.from_to(0, a.length - 1, function(i) {
+				f(a[i]);
+			});
+		}
+	};
+};
